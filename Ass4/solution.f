397405bab097700722500185fc7e67c0f8318c7f
Nimish Gupta <nimish@cs.umass.edu>
2013-10-11 17:09:34 -0400

    not, nand and xor operations for booleans implemented

--- Ass4/solution.f
+++ Ass4/solution.f
@@ -48,3 +48,69 @@ let Proj3 = typfun A ->
                                  z)
 
 ;;
+
+
+type [[Bool]] = forall R . R -> R -> R
+
+;;
+
+let true = typfun R -> fun (x : R) -> fun (y : R) -> x
+
+;;
+
+let false = typfun R -> fun (x : R) -> fun (y : R) -> y
+
+;;
+
+let if = typfun R -> 
+           fun (cond : [[Bool]]) ->
+             fun (true_branch : R) ->
+               fun (false_branch : R) ->
+                 cond<R> true_branch false_branch;;
+
+let and = fun (e1 : [[Bool]]) ->
+            fun (e2 : [[Bool]]) ->
+              typfun R ->
+                fun (x : R) ->
+                  fun (y : R) ->
+                    e1 <R> (e2 <R> x y) y
+
+;;
+
+
+let or = fun (e1 : [[Bool]]) ->
+           fun (e2 : [[Bool]]) ->
+             typfun R ->
+               fun (x : R) ->
+                 fun (y : R) ->
+                   e1 <R> x (e2 <R> x y)
+                   
+;;
+
+
+let not = fun (e : [[Bool]]) ->
+            typfun R ->
+              fun (x : R) ->
+                fun (y : R) ->
+                  e <R> y x
+
+;;
+
+let nand = fun (e1 : [[Bool]]) ->
+            fun (e2 : [[Bool]]) ->
+              typfun R ->
+                fun (t : R) ->
+                  fun (f : R) ->
+                    (not (and e1 e2)) <R> t f
+                    
+;;
+
+let xor = fun (e1 : [[Bool]]) ->
+           fun (e2 : [[Bool]]) ->
+              typfun R ->
+                fun (t : R) ->
+                  fun (f : R) ->
+                    (or (and e1 (not e2)) (and (not e1) e2)) <R> t f
+
+;;
+
