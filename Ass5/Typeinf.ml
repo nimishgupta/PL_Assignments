77a2238e244dedd1fa3655213998c7299b6af9f4
Nimish Gupta <nimish@cs.umass.edu>
2013-10-24 17:33:30 -0400

    constraint generation algo implemented

--- Ass5/Typeinf.ml
+++ Ass5/Typeinf.ml
@@ -32,49 +32,49 @@ let rec from_implicit (exp : I.exp) : E.exp =
 
 (* Step 2. Constraint Generation *)
 
-type constraints = (E.exp * E.typ) list
-
+(* TODO : Make it a map *)
+type constraints = (E.typ * E.typ) list
 
 let cs : constraints ref = ref []
 
+let add_constraint (lhs : E.typ) (rhs : E.typ) : unit =
+  cs := (lhs, rhs)::!cs
 
-let add_constraint (e : E.exp) (t : E.typ) : unit =
-  cs := (e, t)::!cs
+type env = (Id.t * E.typ) list
+
+let lookup (env : env) (x : Id.t) : E.typ = 
+  failwith "NYI"
+
+let augment_env (env : env) (x : Id.t) (t : E.typ) : env =
+  failwith "NYI"
 
 
-let cgen (env : env) (exp : E.exp) : E.typ =
+
+
+let rec cgen (env : env) (exp : E.exp) : E.typ =
   match exp with 
 
-    | E.Int (n) -> add_constraint n E.TInt,
-                   E.TInt
+    | E.Int (n) -> E.TInt
 
-    | E.Bool (b) -> add_constraint b E.TBool,
-                    E.TBool
+    | E.Bool (b) -> E.TBool
 
-    | E.Arith (op, e1, e2) -> cgen env e1,
-                              cgen env e2,
-                              add_constraint e1 E.TInt,
-                              add_constraint e2 E.Tint,
+    | E.Arith (op, e1, e2) -> add_constraint (cgen env e1) E.TInt;
+                              add_constraint (cgen env e2) E.TInt;
                               E.TInt
 
-    | E.Cmp (op, e1, e2) -> cgen env e1,
-                            cgen env e2,
-                            add_constraint e1 E.Int,
-                            add_constraint e2 E.Int,
+    | E.Cmp (op, e1, e2) -> add_constraint (cgen env e1) E.TInt;
+                            add_constraint (cgen env e2) E.TInt;
                             E.TBool
 
-    | E.If (ec, et, ef) -> cgen env ec,
-                           cgen env et,
-                           cgen env ef,
-                           (* Constraints :
+    | E.If (ec, et, ef) -> (* Constraints :
                             *   1. type of ec has to be 'bool'
                             *   2. type of "et" has to be same as that of "ef"
                             *)
-                           add_constraint ec E.TBool,
+                           add_constraint (cgen env ec) E.TBool;
                            let new_t = Id.fresh "t_id" in
-                           add_constraint et new_t,
-                           add_constraint ef new_t,
-                           new_t
+                           add_constraint (cgen env et) (E.TId new_t);
+                           add_constraint (cgen env ef) (E.TId new_t);
+                           E.TId new_t
 
     | E.Id (x) -> (* lookup the type in the environment *)
                   (* XXX : What if there is a free variable? *)
@@ -82,29 +82,55 @@ let cgen (env : env) (exp : E.exp) : E.typ =
 
     | E.Let (x, with_e, in_e) ->  (* Constraints :
                                    * 1. Generate constraints for with_e
-                                   * 1. Assign a new type to with_e
                                    * 2. Assign the same type to x in the environment
-                                   * 3. add constraints for in_e
-                                   * 4. Assign a new type to in_e 
+                                   * 3. add constraints for in_e and return the type of in_e
                                    *)
-                                   cgen env with_e,
-                                   let new_t = Id.fresh (Id.to_string x) in
-                                   let env' = augment_env x new_t in
-                                   add_constraint with_e new_t,
-                                   add_constraint x      new_t,
-                                   cgen env' in_e,
-                                   let in_e_t = Id.fresh "t_id" in
-                                   add_constraint in_e in_e_t,
-                                   in_e_t
+                                   let with_e_t = cgen env with_e in
+                                   let env' = augment_env env x with_e_t in
+                                   cgen env' in_e
 
-    | E.Fun (x, body) -> E.Fun (x, E.TId (Id.fresh (Id.to_string x)), cgen env body)
-    | E.Fix (x, e) -> E.Fix (x, E.TId (Id.fresh (Id.to_string x)), cgen env e)
-    | E.App (e1, e2) -> E.App (cgen env e1, cgen env e2)
-    | E.Empty -> E.Empty (E.TId (Id.fresh "empty"))
-    | E.Cons (e1, e2) -> E.Cons (cgen env e1, cgen env e2)
-    | E.Head (e) -> E.Head (cgen env e)
-    | E.Tail (e) -> E.Tail (cgen env e)
-    | E.IsEmpty (e) -> E.IsEmpty (cgen env e)
-    | E.Pair (e1, e2) -> E.Pair (cgen env e1, cgen env e2)
-    | E.ProjL (e) -> E.ProjL (cgen env e)
-    | E.ProjR (e) -> E.ProjR (cgen env e)
+    | E.Fun (x, x_t, body) -> let env' = augment_env env x x_t in
+                              E.TFun (x_t, cgen env' body)
+
+    | E.Fix (x, x_t, e) -> failwith "NYI"
+
+    | E.App (e1, e2) -> (* e1 has to have a type of function 
+                         * e2 should match the type of the argument expected by function of e1
+                         * type of the App should be the type of e1 body *)
+                        let arg_t  = E.TId (Id.fresh "arg_t") in
+                        let body_t = E.TId (Id.fresh "body_t") in
+                        add_constraint (E.TFun (arg_t,body_t)) (cgen env e1);
+                        add_constraint arg_t (cgen env e2);
+                        body_t
+
+    | E.Empty (t) -> E.TList t
+
+    | E.Cons (e1, e2) -> let t = E.TId (Id.fresh "t") in
+                         add_constraint (E.TList t) (cgen env e2);
+                         add_constraint t (cgen env e1);
+                         E.TList t
+
+    | E.Head (e) -> let t = E.TId (Id.fresh "t") in
+                    add_constraint (E.TList t) (cgen env e);
+                    t
+
+    | E.Tail (e) -> let t = E.TId (Id.fresh "t") in
+                    add_constraint (E.TList t) (cgen env e);
+                    E.TList t
+
+    | E.IsEmpty (e) -> let t = E.TId (Id.fresh "t") in
+                       add_constraint (E.TList t) (cgen env e);
+                       E.TBool
+
+    | E.Pair (e1, e2) -> E.TPair (cgen env e1, cgen env e2)
+
+    | E.ProjL (e) -> let tpl = E.TId (Id.fresh "tpl") in
+                     let tpr = E.TId (Id.fresh "tpr") in
+                     add_constraint (E.TPair (tpl, tpr)) (cgen env e);
+                     tpl
+
+    | E.ProjR (e) -> let tpl = E.TId (Id.fresh "tpl") in
+                     let tpr = E.TId (Id.fresh "tpr") in
+                     add_constraint (E.TPair (tpl, tpr)) (cgen env e);
+                     tpr
+
