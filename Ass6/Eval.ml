5393e9f08c105d48d2d1dbbe18bd31386bd3a075
Nimish Gupta <nimish@cs.umass.edu>
2013-11-26 17:16:15 -0500

    Fairly stable version, all tests are passing as of now

--- Ass6/Eval.ml
+++ Ass6/Eval.ml
@@ -13,7 +13,9 @@ struct
 
 end
 
-type exp_env = exp Env.env
+type exp_env = Env of value Env.env
+and value = exp * exp_env
+
 type typ_env = typ Env.env
 
 
@@ -82,20 +84,19 @@ let type_of (e : exp) : typ = _type_of Env.empty e
 
 type context =  
   | Top
-  | BinOpR      of binOp * exp * context
-  | BinOpL      of binOp * int * context
-  | IfCont      of exp * exp * context
+  | BinOpR      of binOp * exp * exp_env * context
+  | BinOpL      of binOp * int * exp_env * context
+  | IfCont      of exp * exp * exp_env * context
   | LetV        of id * exp * exp_env * context
-  | RestoreEnv  of exp_env * context
-  | AppR        of exp * context
+  | AppR        of exp * exp_env * context
   | AppL        of id * typ * exp * exp_env * context
-  | ConsL       of exp * context
-  | ConsR       of exp * context
-  | HeadCont    of context
-  | TailCont    of context
-  | IsEmptyCont of context
-  | ProjCont    of int * context
-  | TupleCont   of exp list * exp list * context
+  | ConsL       of exp * exp_env * context
+  | ConsR       of exp * exp_env * context
+  | HeadCont    of exp_env * context
+  | TailCont    of exp_env * context
+  | IsEmptyCont of exp_env * context
+  | ProjCont    of int * exp_env * context
+  | TupleCont   of exp list * exp list * exp_env * context
 
 let empty_context : context = Top
 
@@ -103,7 +104,7 @@ let is_empty_context (k : context) : bool = Top = k
   
 
 let rec is_value (e : exp) : bool = match e with
-  | Int _ | Bool _ | Fun _ | Empty _ -> true
+  | Int _ | Bool _  | Fun _ | Empty _ -> true
   | Cons (v1, v2) -> is_value v1 && is_value v2
   | Tuple (vlst) -> List.for_all is_value vlst
   | _ -> false
@@ -127,16 +128,20 @@ let app_relational_op (op : binOp) (nL : int) (nR : int) : bool =
 let step (e : exp) 
          (cont : context)
          (env : exp_env) : exp * context * exp_env = match (e, cont) with
-  | BinOp (op, eL, eR), cont      -> eL, BinOpR (op, eR, cont), env
-  | Int nL, BinOpR (op, eR, cont) -> eR, BinOpL (op, nL, cont), env
-  | Int nR, BinOpL (op, nL, cont) -> (match op with 
-        | Plus | Minus | Times -> Int  (app_arith_op op nL nR), cont, env
-        | LT   | GT    | EQ    -> Bool (app_relational_op op nL nR), cont, env)
 
-  | If (eC, eT, eF), cont -> eC, IfCont (eT, eF, cont), env
-  | Bool b, IfCont (eT, eF, cont) -> if b then eT, cont, env else eF, cont, env
+  | BinOp (op, eL, eR), cont            -> eL, BinOpR (op, eR, env, cont), env
+  | Int nL, BinOpR (op, eR, env', cont) -> eR, BinOpL (op, nL, env', cont), env'
+  | Int nR, BinOpL (op, nL, env', cont) -> (match op with 
+     | Plus | Minus | Times -> Int  (app_arith_op op nL nR), cont, env'
+     | LT   | GT    | EQ    -> Bool (app_relational_op op nL nR), cont, env')
 
-  | Id x, cont -> let v = Env.lookup x env in v, cont, env
+  | If (eC, eT, eF), cont -> eC, IfCont (eT, eF, env, cont), env
+  | Bool b, IfCont (eT, eF, env', cont) -> if b then eT, cont, env' else eF, cont, env'
+
+  | Id x, cont -> 
+      let Env _env = env in 
+      let (v, env') = Env.lookup x _env in
+      v, cont, env'
 
   (* Invariant for Let bindings : We want to restore the environment after evaluating
    * in_e expression
@@ -152,61 +157,59 @@ let step (e : exp)
    *)
   | Let (x, with_e, in_e), cont -> with_e, LetV (x, in_e, env, cont), env
   | v, LetV (x, in_e, env', cont) when is_value v -> 
-      in_e, RestoreEnv (env', cont), Env.bind x v env
-  | v, RestoreEnv (env', cont) when is_value v -> v, cont, env'
+      let Env _env = env' in
+      in_e, cont, Env (Env.bind x (v,env') _env)
 
-
-  | App (e1, e2), cont -> e1, AppR (e2, cont), env
-  | Fun (x, t, body), AppR (e2, cont) -> e2, AppL (x, t, body, env, cont), env
+  | App (e1, e2), cont -> e1, AppR (e2, env, cont), env
+  | Fun (x, t, body), AppR (e2, env', cont) -> e2, AppL (x, t, body, env, cont), env'
   | v, AppL (x, t, body, env', cont) when is_value v && t = type_of v ->
-      body, RestoreEnv (env', cont), Env.bind x v env
+      let Env _env = env' in 
+      body, cont, Env (Env.bind x (v, env) _env)
 
-  (* augment environment by replacing x by a fixpoint *)
-  | Fix (x, _, body), cont -> body, cont, Env.bind x e env
+  | Fix (x, _, body), cont -> let Env _env = env in body, cont, Env (Env.bind x (e,env) _env)
 
-  | Head e, cont -> e, HeadCont (cont), env
-  | v, HeadCont (cont) when is_value v -> (match v with
-      | Empty _ -> failwith "Head of empty list requested"
+  | Head e, cont -> e, HeadCont (env, cont), env
+  | v, HeadCont (env', cont) when is_value v -> (match v with
       | Cons (atm, lst) -> (match type_of atm, type_of lst with 
-          | t1, TList t2 when t1 = t2 -> atm, cont, env
+          | t1, TList t2 when t1 = t2 -> atm, cont, env'
           | _ -> failwith "Type Error : Cons type mismatch")
+      | Empty _ -> failwith "Head of empty list requested"
       | _ -> failwith "Type Error : Expected list")
 
-  | Tail e, cont -> e, TailCont (cont), env
-  | v, TailCont (cont) when is_value v -> (match v with
-      | Empty _ -> failwith "Tail of empty list requested"
+  | Tail e, cont -> e, TailCont (env, cont), env
+  | v, TailCont (env', cont) when is_value v -> (match v with
       | Cons (atm, lst) -> (match type_of atm, type_of lst with
-          | t1, TList t2 when t1 = t2 -> lst, cont, env
+          | t1, TList t2 when t1 = t2 -> lst, cont, env'
           | _ -> failwith "Type Error : Cons type mismatch")
+      | Empty _ -> failwith "Tail of empty list requested"
       | _ -> failwith "Type Error : Expected list")
 
-  | IsEmpty e, cont -> e, IsEmptyCont (cont), env
-  | v, IsEmptyCont (cont) when is_value v -> (match v with
-     | Empty _ -> Bool true, cont, env
-     | Cons (atm, lst) -> (match type_of atm, type_of lst with
-         | t1, TList t2 when t1 = t2 -> Bool false, cont, env
-         | _ -> failwith "Type Error : Cons Type mismatch")
+  | IsEmpty e, cont -> e, IsEmptyCont (env, cont), env
+  | v, IsEmptyCont (env', cont) when is_value v -> (match v with
+     | Cons _  -> Bool false, cont, env'
+     | Empty _ -> Bool true, cont, env'
      | _ -> failwith "Expected list")
 
-  | Cons (atm, lst), cont when not (is_value atm && is_value lst) -> atm, ConsR (lst, cont), env
-  | vatm, ConsR (lst, cont) when is_value vatm -> lst, ConsL (vatm, cont), env
-  | vlst, ConsL (vatm, cont) when is_value vlst -> Cons (vatm, vlst), cont, env
+  | Cons (atm, lst), cont when not (is_value atm && is_value lst) -> 
+      atm, ConsR (lst, env, cont), env
+  | vatm, ConsR (lst,  env', cont) when is_value vatm -> lst, ConsL (vatm, env', cont), env'
+  | vlst, ConsL (vatm, env', cont) when is_value vlst -> Cons (vatm, vlst), cont, env'
 
 
-  | Proj (e, n), cont -> e, ProjCont (n, cont), env
-  | v, ProjCont (n, cont) when is_value v -> (match v with
-      | Tuple (vlist) -> List.nth vlist n, cont, env
+  | Proj (e, n), cont -> e, ProjCont (n, env, cont), env
+  | v, ProjCont (n, env', cont) when is_value v -> (match v with
+      | Tuple (vlist) -> List.nth vlist n, cont, env'
       | _ -> failwith "Expected Tuple")
 
 
   (* Base case *)
   | Tuple (elist), cont when not (is_value e) ->
-      let open List in hd elist, TupleCont (tl elist, [], cont), env
+      let open List in hd elist, TupleCont (tl elist, [], env, cont), env
 
   (* General case *)
-  | v, TupleCont (elist, vlist, cont) when is_value v -> let open List in
-      if [] = elist then Tuple (rev (v::vlist)), cont, env
-      else hd elist, TupleCont (tl elist, v::vlist, cont), env
+  | v, TupleCont (elist, vlist, env', cont) when is_value v -> let open List in
+      if [] = elist then Tuple (rev (v::vlist)), cont, env'
+      else hd elist, TupleCont (tl elist, v::vlist, env', cont), env'
 
   | Read (typ), cont -> (match M_util.parse (read_line ()) with
      | M_util.ParseError err -> failwith err
@@ -223,7 +226,7 @@ let rec run (e : exp) (cont : context) (env : exp_env) = match (e, cont) with
   | v, Top when is_value v -> v
   | e, k -> let (e', k', env') = step e k env in run e' k' env' 
 
-let eval (e : exp) : exp = let _ = type_of e in run e empty_context Env.empty
+let eval (e : exp) : exp = let _ = type_of e in run e empty_context (Env (Env.empty))
 
 
 
@@ -271,13 +274,48 @@ let exec_exp (e_str : string) : exp = match M_util.parse e_str with
   | M_util.ParseError msg -> failwith msg
 
 
-let _ = exec_exp "let x = 300 in (let x = 50 in x) + x"
-
-
+(* Check if correct exp evaluates fine *)
 TEST = exec_exp "3"     = Int (3)
 TEST = exec_exp "true"  = Bool (true)
 TEST = exec_exp "false" = Bool (false)
 TEST = exec_exp "if true then 1 else 0" = Int (1)
 TEST = exec_exp "if false then 1 else 0" = Int (0)
 
+(* Bin op *)
+TEST = exec_exp "3 + 5" = Int (8)
+TEST = exec_exp "3 - 5" = Int (-2)
+TEST = exec_exp "3 * 5" = Int (15)
+TEST = exec_exp "3 < 5" = Bool (true)
+TEST = exec_exp "3 > 5" = Bool (false)
+TEST = exec_exp "3 = 5" = Bool (false)
+
+(* composite *)
+TEST = exec_exp "if true then 1 + 2 else 3 + 5" = Int (3)
+
+(* env check : Nested Let *)
+TEST = exec_exp "let x = 5 in let y = 6 in x + y" = Int (11)
+TEST = exec_exp "let y = 5 in let x = 6 in x + y" = Int (11)
+
+TEST = exec_exp "let x = 11 in x + (let x = 5 in x) + (let x = 8 in x)" = Int (24)
+
+TEST = exec_exp "let double = fun (x : int) -> 2 * x in double 5" =
+       exec_exp "let double = fun (n : int) -> n + n in double 5"
+
+
+(* lazy use of let binding *)
+TEST = exec_exp "(let x = 5 in fun (y : int) -> x * y) 2" = Int (10)
+
+(* Check currying *)
+TEST = exec_exp "let add = fun (x : int) -> fun (y : int) -> x + y in (add 3) 5" = Int (8)
+
+(* TODO : Check Function pass *)
+(* TODO : Check function return *)
+(* TODO : Check closure *)
+
+
+(* class test case *)
+TEST = exec_exp "let x = 300 in (let x = 50 in x) + x" = Int (350)
+
+
+(* Check if incorret doesn't evaluate  *)
 
