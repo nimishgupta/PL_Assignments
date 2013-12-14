320f99892a02e551a45e323eb4234af1d3f65659
Nimish Gupta <nimish28@gmail.com>
2013-12-13 20:46:34 -0500

    program is working for now

--- Ass7/Eval.ml
+++ Ass7/Eval.ml
@@ -82,28 +82,6 @@ let rec _type_of (env : typ_env) (e : exp) : typ = match e with
 let type_of (e : exp) : typ = _type_of Env.empty e
 
 
-type context =  
-  | Top
-  | BinOpR      of binOp * exp * exp_env * context
-  | BinOpL      of binOp * int * exp_env * context
-  | IfCont      of exp * exp * exp_env * context
-  | LetV        of id * exp * exp_env * context
-  | AppR        of exp * exp_env * context
-  | AppL        of id * typ * exp * exp_env * exp_env * context
-  | ConsL       of exp * exp_env * context
-  | ConsR       of exp * exp_env * context
-  | HeadCont    of exp_env * context
-  | TailCont    of exp_env * context
-  | IsEmptyCont of exp_env * context
-  | ProjCont    of int * exp_env * context
-  | TupleCont   of exp list * exp list * exp_env * context
-  | WriteCont   of exp_env * context
-
-let empty_context : context = Top
-
-let is_empty_context (k : context) : bool = Top = k
-  
-
 let rec is_value (e : exp) : bool = match e with
   | Int _ | Bool _  | Fun _ | Empty _ -> true
   | Cons (v1, v2) -> is_value v1 && is_value v2
@@ -126,116 +104,319 @@ let app_relational_op (op : binOp) (nL : int) (nR : int) : bool =
     | _ -> failwith "Invalid op"
 
 
-let step (e : exp) 
-         (cont : context)
-         (env : exp_env) : exp * context * exp_env = match (e, cont) with
 
-  | BinOp (op, eL, eR), cont            -> eL, BinOpR (op, eR, env, cont), env
-  | Int nL, BinOpR (op, eR, env', cont) -> eR, BinOpL (op, nL, env', cont), env'
-  | Int nR, BinOpL (op, nL, env', cont) -> (match op with 
-     | Plus | Minus | Times -> Int  (app_arith_op op nL nR), cont, env'
-     | LT   | GT    | EQ    -> Bool (app_relational_op op nL nR), cont, env')
+(*
+module type IO = sig
+  val read : unit -> string
+  val write : Exp.exp -> unit
+end
 
-  | If (eC, eT, eF), cont -> eC, IfCont (eT, eF, env, cont), env
-  | Bool b, IfCont (eT, eF, env', cont) -> if b then eT, cont, env' else eF, cont, env'
+module Continuation (IO : IO) = struct
+*)
 
-  | Id x, cont -> 
-      (* print_string ("Looking for : " ^ (Identifier.to_string x) ^ "\n"); *)
-      let Env _env = env in 
-      let (v, env') = Env.lookup x _env in
-      v, cont, env'
+  type context =  
+    | Top
+    | BinOpR      of binOp * exp * exp_env * context
+    | BinOpL      of binOp * int * exp_env * context
+    | IfCont      of exp * exp * exp_env * context
+    | LetV        of id * exp * exp_env * context
+    | AppR        of exp * exp_env * context
+    | AppL        of id * typ * exp * exp_env * exp_env * context
+    | ConsL       of exp * exp_env * context
+    | ConsR       of exp * exp_env * context
+    | HeadCont    of exp_env * context
+    | TailCont    of exp_env * context
+    | IsEmptyCont of exp_env * context
+    | ProjCont    of int * exp_env * context
+    | TupleCont   of exp list * exp list * exp_env * context
+    | WriteCont   of exp_env * context
 
-  (* Invariant for Let bindings : We want to restore the environment after evaluating
-   * in_e expression
-   *
-   * Consider the two cases
-   *   Let x = 1 in 
-   *     Let y = 2 in
-   *       x + y
-   *
-   *   Let y = 2 in
-   *     Let x = 1 in
-   *       x + y
-   *)
-  | Let (x, with_e, in_e), cont -> with_e, LetV (x, in_e, env, cont), env
-  | v, LetV (x, in_e, env', cont) when is_value v -> 
-      (match v with
-        (* If with_e evals to a function-value then we need to preserve its environment *)
-        | Fun _ -> let Env _env = env  in in_e, cont, Env (Env.bind x (v, env)  _env)
-        | _     -> let Env _env = env' in in_e, cont, Env (Env.bind x (v, env') _env))
+  let empty_context : context = Top
 
-  | App (e1, e2), cont -> e1, AppR (e2, env, cont), env
-  | Fun (x, t, body), AppR (e2, env', cont) -> e2, AppL (x, t, body, env, env', cont), env'
-  | v, AppL (x, t, body, fenv, orig_env, cont) when is_value v && t = type_of v ->
-      let Env _fenv = fenv in 
-      body, cont, Env (Env.bind x (v, orig_env) _fenv)
+  let is_empty_context (k : context) : bool = Top = k
+  
 
-  | Fix (x, _, body), cont -> 
-      let Env _env = env in
-      body, cont, Env (Env.bind x (e,env) _env)
+  let step (e : exp) 
+           (cont : context)
+           (env : exp_env) : exp * context * exp_env = match (e, cont) with
 
-  | Head e, cont -> e, HeadCont (env, cont), env
-  | v, HeadCont (env', cont) when is_value v -> (match v with
-      | Cons (atm, lst) -> (match type_of atm, type_of lst with 
-          | t1, TList t2 when t1 = t2 -> atm, cont, env'
-          | _ -> failwith "Type Error : Cons type mismatch")
-      | Empty _ -> failwith "Head of empty list requested"
-      | _ -> failwith "Type Error : Expected list")
+    | BinOp (op, eL, eR), cont            -> eL, BinOpR (op, eR, env, cont), env
+    | Int nL, BinOpR (op, eR, env', cont) -> eR, BinOpL (op, nL, env', cont), env'
+    | Int nR, BinOpL (op, nL, env', cont) -> (match op with 
+        | Plus | Minus | Times -> Int  (app_arith_op op nL nR), cont, env'
+        | LT   | GT    | EQ    -> Bool (app_relational_op op nL nR), cont, env')
 
-  | Tail e, cont -> e, TailCont (env, cont), env
-  | v, TailCont (env', cont) when is_value v -> (match v with
-      | Cons (atm, lst) -> (match type_of atm, type_of lst with
-          | t1, TList t2 when t1 = t2 -> lst, cont, env'
-          | _ -> failwith "Type Error : Cons type mismatch")
-      | Empty _ -> failwith "Tail of empty list requested"
-      | _ -> failwith "Type Error : Expected list")
+    | If (eC, eT, eF), cont -> eC, IfCont (eT, eF, env, cont), env
+    | Bool b, IfCont (eT, eF, env', cont) -> if b then eT, cont, env' else eF, cont, env'
 
-  | IsEmpty e, cont -> e, IsEmptyCont (env, cont), env
-  | v, IsEmptyCont (env', cont) when is_value v -> (match v with
-     | Cons _  -> Bool false, cont, env'
-     | Empty _ -> Bool true, cont, env'
-     | _ -> failwith "Expected list")
+    | Id x, cont -> 
+        let Env _env = env in 
+        let (v, env') = Env.lookup x _env in
+        v, cont, env'
 
-  | Cons (atm, lst), cont when not (is_value atm && is_value lst) -> 
-      atm, ConsR (lst, env, cont), env
-  | vatm, ConsR (lst,  env', cont) when is_value vatm -> lst, ConsL (vatm, env', cont), env'
-  | vlst, ConsL (vatm, env', cont) when is_value vlst -> Cons (vatm, vlst), cont, env'
+    | Let (x, with_e, in_e), cont -> with_e, LetV (x, in_e, env, cont), env
+    | v, LetV (x, in_e, env', cont) when is_value v -> 
+        (match v with
+          (* If with_e evals to a function-value then we need to preserve its environment *)
+          | Fun _ -> let Env _env = env  in in_e, cont, Env (Env.bind x (v, env)  _env)
+          | _     -> let Env _env = env' in in_e, cont, Env (Env.bind x (v, env') _env))
+
+    | App (e1, e2), cont -> e1, AppR (e2, env, cont), env
+    | Fun (x, t, body), AppR (e2, env', cont) -> e2, AppL (x, t, body, env, env', cont), env'
+    | v, AppL (x, t, body, fenv, orig_env, cont) when is_value v && t = type_of v ->
+        let Env _fenv = fenv in 
+        body, cont, Env (Env.bind x (v, orig_env) _fenv)
+
+    | Fix (x, _, body), cont -> 
+        let Env _env = env in
+        body, cont, Env (Env.bind x (e,env) _env)
+
+    | Head e, cont -> e, HeadCont (env, cont), env
+    | v, HeadCont (env', cont) when is_value v -> (match v with
+        | Cons (atm, lst) -> (match type_of atm, type_of lst with 
+            | t1, TList t2 when t1 = t2 -> atm, cont, env'
+            | _ -> failwith "Type Error : Cons type mismatch")
+        | Empty _ -> failwith "Head of empty list requested"
+        | _ -> failwith "Type Error : Expected list")
+
+    | Tail e, cont -> e, TailCont (env, cont), env
+    | v, TailCont (env', cont) when is_value v -> (match v with
+        | Cons (atm, lst) -> (match type_of atm, type_of lst with
+            | t1, TList t2 when t1 = t2 -> lst, cont, env'
+            | _ -> failwith "Type Error : Cons type mismatch")
+        | Empty _ -> failwith "Tail of empty list requested"
+        | _ -> failwith "Type Error : Expected list")
+
+    | IsEmpty e, cont -> e, IsEmptyCont (env, cont), env
+    | v, IsEmptyCont (env', cont) when is_value v -> (match v with
+        | Cons _  -> Bool false, cont, env'
+        | Empty _ -> Bool true, cont, env'
+        | _ -> failwith "Expected list")
+
+    | Cons (atm, lst), cont when not (is_value atm && is_value lst) -> 
+        atm, ConsR (lst, env, cont), env
+    | vatm, ConsR (lst,  env', cont) when is_value vatm -> lst, ConsL (vatm, env', cont), env'
+    | vlst, ConsL (vatm, env', cont) when is_value vlst -> Cons (vatm, vlst), cont, env'
 
 
-  | Proj (e, n), cont -> e, ProjCont (n, env, cont), env
-  | v, ProjCont (n, env', cont) when is_value v -> (match v with
-      | Tuple (vlist) -> List.nth vlist n, cont, env'
-      | _ -> failwith "Expected Tuple")
+    | Proj (e, n), cont -> e, ProjCont (n, env, cont), env
+    | v, ProjCont (n, env', cont) when is_value v -> (match v with
+        | Tuple (vlist) -> List.nth vlist n, cont, env'
+        | _ -> failwith "Expected Tuple")
 
 
   (* Base case *)
-  | Tuple (elist), cont when not (is_value e) ->
-      let open List in hd elist, TupleCont (tl elist, [], env, cont), env
+    | Tuple (elist), cont when not (is_value e) ->
+        let open List in hd elist, TupleCont (tl elist, [], env, cont), env
 
   (* General case *)
-  | v, TupleCont (elist, vlist, env', cont) when is_value v -> let open List in
-      if [] = elist then Tuple (rev (v::vlist)), cont, env'
-      else hd elist, TupleCont (tl elist, v::vlist, env', cont), env'
+    | v, TupleCont (elist, vlist, env', cont) when is_value v -> let open List in
+        if [] = elist then Tuple (rev (v::vlist)), cont, env'
+        else hd elist, TupleCont (tl elist, v::vlist, env', cont), env'
 
-  | Read (typ), cont -> (match M_util.parse (read_line ()) with
-     | M_util.ParseError err -> failwith err
-     | M_util.Exp e' when is_value e' && typ = type_of e' -> e', cont, env
-     | _ -> failwith "Not a value")
+    (* TODO : Refactor *)
+    | Read (typ), cont -> (match M_util.parse (read_line ()) with
+        | M_util.ParseError err -> failwith err
+        | M_util.Exp e' when is_value e' && typ = type_of e' -> e', cont, env
+        | _ -> failwith "Not a value")
 
-  | Write (e), cont -> e, WriteCont (env, cont), env
+    | Write (e), cont -> e, WriteCont (env, cont), env
 
-  (* XXX : what exp to return when printing? *)
-  | v, WriteCont (env', cont) when is_value v -> 
-      M_util.print_exp v; print_newline (); v, cont, env'
+    (* XXX : what exp to return when printing? *)
+    | v, WriteCont (env', cont) when is_value v -> 
+        M_util.print_exp v; print_newline (); v, cont, env'
 
-  | _ -> failwith "Unexpected Expression : Invalid Expression/Type"
+    | _ -> failwith "Unexpected Expression : Invalid Expression/Type"
+
+
 
 
 let rec run (e : exp) (cont : context) (env : exp_env) = match (e, cont) with
   | v, Top when is_value v -> v
   | e, k -> let (e', k', env') = step e k env in run e' k' env' 
 
+
+
+
+
+let to_result (e : M_syntax.exp) : string = 
+  "<HTML><BODY>" ^ (M_util.string_of_exp e) ^ "</BODY></HTML>"
+
+
+let to_hexchar (i : int) : char =
+  assert (i >= 0 && i <= 15);
+  let base = if i >= 0 && i <= 9 
+             then Char.code '0' 
+             else (Char.code 'a') - 10 in
+  Char.chr (base + i)
+
+
+let char_to_hexstring (c : Char.t) : string = 
+  let code = Char.code c in 
+  let highc = to_hexchar ((code lsr 4) land 0xF) in
+  let lowc  = to_hexchar (code land 0xF) in
+  (String.make 1 highc) ^ (String.make 1 lowc)
+
+let bytes_to_hexstring (b : string) : string =
+  let r = String.create ((String.length b) * 2) in
+  String.iteri (fun i c -> String.blit (char_to_hexstring c) 0 r (i*2) 2) b;
+  r
+
+let hexchar_to_int (c : Char.t): int =
+  let open Char in
+  if code c >= code '0' && code c <= code '9' then (code c - code '0')
+  else if code c >= code 'a' && code c <= code 'f' then (code c - code 'a' + 10)
+  else begin print_endline (String.make 1 c); failwith "Not a hexadecimal character" end
+  
+
+let hexs_to_byte (highc : Char.t) (lowc: Char.t) : Char.t =
+  let highi = hexchar_to_int highc in 
+  let lowi  = hexchar_to_int lowc  in
+  Char.chr ((highi lsl 4) lor lowi)
+
+let hexstring_to_bytes (h : string) : string = 
+  let l = String.length h in 
+  assert (l > 0 && (l mod 2) = 0);
+  let byte_str = String.create (l/2) in
+  String.iteri (fun i _ -> 
+                  if (i mod 2) = 0
+                  then let highc, lowc = h.[i], h.[i+1] in byte_str.[i/2] <- (hexs_to_byte highc lowc)) h;
+  byte_str
+  
+
+let to_form (t : M_syntax.typ) (env : exp_env) (k : context): string  =
+  assert (env = (Marshal.from_string (hexstring_to_bytes (bytes_to_hexstring (Marshal.to_string env []))) 0));
+  assert (k   = (Marshal.from_string (hexstring_to_bytes (bytes_to_hexstring (Marshal.to_string k   []))) 0));
+  let env_str = bytes_to_hexstring (Marshal.to_string env []) in
+  let cont_str = bytes_to_hexstring (Marshal.to_string k []) in
+  print_endline env_str; print_endline cont_str;
+  "<HTML><BODY> 
+      <form name=\"myform\" action=\"\" method=\"POST\">
+      <input type=\"hidden\" name=\"env\" value=\"" ^ env_str ^ "\">
+      <input type=\"hidden\" name=\"con\" value=\"" ^ cont_str ^ "\">
+      <input type=\"text\"   name=\"param\" value=\"\">
+      </form>
+  </BODY<</HTML>"
+
+      
+
+let rec run_server (e : exp) (cont : context) (env : exp_env) : string = 
+  match (e, cont) with
+    | v, Top when is_value v -> to_result v
+    | Read (typ), cont -> to_form typ env cont
+    | e, k -> let (e', k', env') = step e k env in run_server e' k' env'
+
+
 let eval (e : exp) : exp = let _ = type_of e in run e empty_context (Env (Env.empty))
+
+
+(* TODO : Sanity check on port *)
+
+
+let rec split (str : string) (sep : Char.t) : string list =
+  let open String in
+    try let i = index str sep
+        in (sub str 0 i) :: split (sub str (i+1) ((length str) - (i+1))) sep
+    with Not_found -> [str]
+
+(* open Core.Std *)
+(* open Async.Std *)
+
+module Request = Cohttp.Request
+module Server  = Cohttp_async.Server
+
+(* TODO : Refactor *)
+let handle_body (body : string) : Server.response Async.Std.Deferred.t =
+  let varlst = split body '&' in
+  print_endline body; print_endline (string_of_int (List.length varlst));
+  if List.length varlst <> 3 
+  then Server.respond_with_string "Corrupt request" ~code: `Bad_request
+  else
+    begin
+      let assoclst = List.map (fun el -> let l = split el '=' in List.hd l, List.hd (List.tl l)) varlst
+      in (* try *)
+           let env, con = Marshal.from_string (hexstring_to_bytes (List.assoc "env" assoclst)) 0,
+                          Marshal.from_string (hexstring_to_bytes (List.assoc "con" assoclst)) 0
+           in (match M_util.parse (List.assoc "param" assoclst) with
+                 | M_util.Exp e -> 
+                     (* TODO : MAybe check for exceptions *)
+                     Server.respond_with_string (run_server e con env) ~code: `OK
+                 | M_util.ParseError msg -> 
+                     Server.respond_with_string ("Error: " ^ msg) ~code: `Bad_request)
+
+         (* with _ -> Server.respond_with_string "Corrup request" ~code: `Bad_request *)
+    end
+
+
+open Async.Std
+(* This shouldn't be necessary, but let's you handle lots of data:
+ 
+   http://en.wikipedia.org/wiki/Chunked_transfer_encoding *)
+let cat_chunks (body : string Pipe.Reader.t) : string Deferred.t =
+  Pipe.fold body ~init:"" ~f:(fun x y -> Deferred.return (x ^ y))
+
+
+let handle_client (e : M_syntax.exp)
+                  ~(body : string Pipe.Reader.t option)
+                  (client_addr : Socket.Address.Inet.t)
+                  (request : Request.t) : Server.response Deferred.t =
+  let open Request in match request.meth with
+    | `GET -> Server.respond_with_string (run_server e empty_context (Env (Env.empty))) ~code: `OK
+    | `POST -> (match body with
+        | None -> Server.respond_with_string "missing body" ~code: `Bad_request
+        | Some body -> cat_chunks body >>= handle_body)
+    | _ -> Server.respond_with_string "" ~code: `Bad_request
+
+    
+
+let run (port : int) (e : M_syntax.exp) = 
+  Server.create (Tcp.on_port port) (handle_client e)
+
+
+(*
+let rec repl () = 
+  print_string "> ";
+  (match M_util.parse (read_line ()) with
+     | M_util.Exp exp -> 
+         let v = eval exp in
+         eval (Write v)
+
+    | M_util.ParseError msg ->
+        print_endline msg);
+  repl ()
+*)
+
+
+let usage = "cs691f run Server [ repl | FILENAME | PORT FILENAME ]"
+
+let _ =  
+  match Array.to_list Sys.argv with
+  (*  | [ exe; "repl" ] -> print_string "Press Ctrl + C to quit.\n"; repl () *)
+    
+    (* parse from file specified on command line *)
+    | [ exe; f] -> (match (M_util.parse_from_file f) with
+        | M_util.Exp exp ->
+            let v = eval exp in
+            let _ = eval (Write v) in ()
+
+        | M_util.ParseError msg -> print_endline msg)
+
+    | [ exe; port; f] -> (match (M_util.parse_from_file f) with
+        | M_util.ParseError msg -> print_endline msg
+
+        | M_util.Exp e -> 
+              let _ = run (int_of_string port) e in
+              Core.Std.never_returns (Scheduler.go ()))
+
+    | _ -> print_endline usage
+
+let exec_exp (e_str : string) : exp = match M_util.parse e_str with 
+  | M_util.Exp exp -> eval exp
+  | M_util.ParseError msg -> failwith msg
+
+
+
+
 
 
 
@@ -244,43 +425,6 @@ let eval (e : exp) : exp = let _ = type_of e in run e empty_context (Env (Env.em
  *                                       TESTS                                           *
  *                                                                                       *
  *****************************************************************************************)
-
-
-let rec repl () = 
-  print_string "> ";
-  match M_util.parse (read_line ()) with
-    | M_util.Exp exp -> 
-        let v = eval exp in
-        let _ = eval (Write v) in
-        repl ()
-
-    | M_util.ParseError msg ->
-        print_string msg;
-        print_newline ();
-        repl ()
-
-
-let _ =  
-  match Array.to_list Sys.argv with
-    | [ exe; "repl" ] -> print_string "Press Ctrl + C to quit.\n"; repl ()
-    
-    (* parse from file specified on command line *)
-    | [ exe; f] -> (match (M_util.parse_from_file f) with
-
-                     | M_util.Exp exp ->
-                       let v = eval exp in
-                       let _ = eval (Write v) in ()
-
-                     | M_util.ParseError msg -> 
-                        print_string msg;
-                        print_newline ()
-                   )
-    | _ -> ()
-
-
-let exec_exp (e_str : string) : exp = match M_util.parse e_str with 
-  | M_util.Exp exp -> eval exp
-  | M_util.ParseError msg -> failwith msg
 
 
 (* Check if correct exp evaluates fine *)
