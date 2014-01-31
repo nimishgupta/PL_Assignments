e5527e74cf098e4d039ddb4df61d27e093950d51
Nimish Gupta <nimish28@gmail.com>
2014-01-31 16:14:13 -0500

    pair equality resolved

--- Spring/Ass2/typed_pairs.v
+++ Spring/Ass2/typed_pairs.v
@@ -34,7 +34,9 @@ Definition make_pair targ1 targ2 (l : targ1) (r : targ2): (targ1 * targ2) :=
 
 (* Maybe typeDenote is causing a problem *)
 (* Need to define recursive define for pairs *)
-Definition eqp (t1 t2: type) (p1 p2: typeDenote (Pair t1 t2)) : typeDenote Bool :=
+Section equality.
+Variables A B: Type.
+Definition eqp (p1 p2: A * B) : bool :=
   let (a1, b1) := p1 in
   let (a2, b2) := p2 in
   (* (TEq _ a1 a2) andb (TEq _ b1 b2) *)
@@ -43,12 +45,24 @@ Definition eqp (t1 t2: type) (p1 p2: typeDenote (Pair t1 t2)) : typeDenote Bool 
 Eval simpl in typeDenote (Pair Nat Bool).
 Check eqp.
 Check pair.
-Check eqp (3, false) (4, true).
-Eval simpl in eqp (3, false) (4, true). 
+(* Check eqp (3, false) (4, true). *)
+(* Eval simpl in eqp (3, false) (4, true). *) 
 
 (* Check typeDenote (Pair _ _). *)
 
-Definition tbinopDenote targ1 targ2 tres (b: tbinop targ1 targ2 tres)
+Definition beq_pair {a b : Type} (eqA : a -> a -> bool) (eqB : b -> b -> bool) (x y: a*b) : bool
+    := let (xa, xb) := x in let (ya, yb) := y in eqA xa ya && eqB xb yb.
+
+
+Fixpoint my_lt (m n: nat) : bool :=
+  match m, n with
+    | O, O => true
+    | O, _ => false
+    | _, O => false
+    | S m', S n' => my_lt m' n'
+  end.
+
+Fixpoint tbinopDenote targ1 targ2 tres (b: tbinop targ1 targ2 tres)
   : typeDenote targ1 -> typeDenote targ2 -> typeDenote tres :=
   match b in tbinop targ1 targ2 tres 
     return typeDenote targ1 -> typeDenote targ2 -> typeDenote tres with
@@ -57,11 +71,9 @@ Definition tbinopDenote targ1 targ2 tres (b: tbinop targ1 targ2 tres)
     | TTimes    => mult
     | TEq Nat   => beq_nat
     | TEq Bool  => eqb
-    | TLt       => leb
-    (* | TEq ((* Should we have a forall clause *) Pair t1 t2)  => eqp *)
-    | TEq (Pair t1 t2)  => eqp t1 t2
-    (* Check if make_pair has to be a dependent type *)
+    | TEq (Pair t1 t2)  => beq_pair (tbinopDenote (TEq t1)) (tbinopDenote (TEq t2))
     | TMakePair _ _ => make_pair
+    | TLt       => (* leb *) my_lt
   end.
 
 
@@ -87,7 +99,6 @@ Eval simpl in typeDenote (Pair Bool (Pair Bool Nat)).
 
 
 
-
 Definition projL targ tres (p : targ) : tres :=
   let (l, _) := p in l.
 
@@ -108,3 +119,8 @@ Definition texpDenote t (e: texp t): typeDenote t :=
     | TBinop _ _ _ op e1 e2 => (tbinopDenote op) (texpDenote e1) (texpDenote e2)
     | TPairop _ _ op e' => (tpairopDenote op) (texpDenote e')
   end.
+
+(* TODO : Some tests and Eval examples *)
+
+
+
