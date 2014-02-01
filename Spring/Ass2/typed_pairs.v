72995cf5cc6c8265ae3475219f18a9ee42aea808
Nimish Gupta <nimish28@gmail.com>
2014-02-01 00:53:28 -0500

    Almost complete

--- Spring/Ass2/typed_pairs.v
+++ Spring/Ass2/typed_pairs.v
@@ -32,7 +32,7 @@ Definition beq_pair {a b : Type} (eqA : a -> a -> bool) (eqB : b -> b -> bool) (
     := let (xa, xb) := x in let (ya, yb) := y in eqA xa ya && eqB xb yb.
 
 
-(* Need to make this function go away *)
+(* TODO : Need to make this function go away *)
 Fixpoint my_lt (m n: nat) : bool :=
   match m, n with
     | O, O => true
@@ -56,29 +56,10 @@ Definition tbinopDenote targ1 targ2 tres (b: tbinop targ1 targ2 tres)
     | TPlus  => plus
     | TTimes => mult
     | TEq t => type_comparator t
+    (* TODO : Need to make this function go away *)
     | TLt    => (* leb *) my_lt
     | TMakePair _ _ => pair
   end.
-
-
-
-
-Fixpoint tbinopDenote targ1 targ2 tres (b: tbinop targ1 targ2 tres)
-  : typeDenote targ1 -> typeDenote targ2 -> typeDenote tres :=
-  match b in tbinop targ1 targ2 tres
-    return typeDenote targ1 -> typeDenote targ2 -> typeDenote tres with
-    | TPlus  => plus
-    | TTimes => mult
-    | TEq Nat => beq_nat
-    | TEq Bool => eqb
-    (* XXX : coq not convinced that we are disintegrating into smaller arguments *)
-    | TEq (Pair t1 t2) => beq_pair (tbinopDenote (TEq t1)) (tbinopDenote (TEq t2))
-    | TLt    => (* leb *) my_lt
-    | TMakePair _ _ => pair
-  end.
-
-
-(********************************************************************************************)
 
 
 (* Pair operations *)
@@ -94,24 +75,21 @@ Inductive texp : type -> Set :=
   | TBinop: forall t1 t2 t, tbinop t1 t2 t -> texp t1 -> texp t2 -> texp t
   | TPairop: forall t1 t2, tpairop t1 t2 -> texp t1 -> texp t2.
 
-Eval simpl in typeDenote (Pair Bool (Pair Bool Nat)).
 
-
-
-Definition projL targ tres (p : targ) : tres :=
+Definition projL {a b: Type} (p : a*b) : a :=
   let (l, _) := p in l.
 
-Definition projR targ tres (p : targ) : tres :=
+Definition projR {a b: Type} (p : a*b) : b :=
   let (_, r) := p in r.
 
 Definition tpairopDenote targ1 tres (op : tpairop targ1 tres)
  : typeDenote targ1 -> typeDenote tres :=
   match op with
-    | ProjL => projL
-    | ProjR => projR
+    | ProjL _ _ => projL
+    | ProjR _ _ => projR
   end.
 
-Definition texpDenote t (e: texp t): typeDenote t :=
+Fixpoint texpDenote t (e: texp t): typeDenote t :=
   match e with
     | TNConst n => n
     | TBConst b => b
@@ -119,7 +97,89 @@ Definition texpDenote t (e: texp t): typeDenote t :=
     | TPairop _ _ op e' => (tpairopDenote op) (texpDenote e')
   end.
 
-(* TODO : Some tests and Eval examples *)
+Eval simpl in texpDenote (TNConst 42).
+Eval simpl in texpDenote (TBConst true).
+Eval simpl in texpDenote (TBinop (TMakePair Nat Bool) (TNConst 42) (TBConst true)).
+Eval simpl in texpDenote (TBinop TTimes (TBinop TPlus (TNConst 2) (TNConst 2)) (TNConst 7)).
+Eval simpl in texpDenote (TBinop TTimes 
+                                   (TPairop (ProjL Nat Nat) 
+                                     (TBinop (TMakePair Nat Nat) (TNConst 7) (TNConst 2)))
+                                   (TPairop (ProjR Nat Nat) 
+                                     (TBinop (TMakePair Nat Nat) (TNConst 7) (TNConst 2)))).
+Eval simpl in texpDenote (TBinop (TEq Nat) (TBinop TPlus (TNConst 2) (TNConst 2)) (TNConst 7)).
+Eval simpl in texpDenote (TBinop (TEq Nat) (TNConst 8) (TBinop TTimes (TNConst 4) (TNConst 2))).
+Eval simpl in texpDenote (TBinop (TEq (Pair (Pair Nat Bool) Bool))
+                                    (TBinop (TMakePair (Pair Nat Bool) Bool) 
+                                                  (TBinop (TMakePair Nat Bool) (TNConst 4) (TBConst true))
+                                                  (TBConst false))
+                                    (TBinop (TMakePair (Pair Nat Bool) Bool) 
+                                                  (TBinop (TMakePair Nat Bool) (TNConst 3) (TBConst true))
+                                                  (TBConst false))).
+
+Eval simpl in texpDenote (TBinop (TEq (Pair (Pair Nat Bool) Bool))
+                                    (TBinop (TMakePair (Pair Nat Bool) Bool) 
+                                                  (TBinop (TMakePair Nat Bool) (TNConst 4) (TBConst true))
+                                                  (TBConst false))
+                                    (TBinop (TMakePair (Pair Nat Bool) Bool) 
+                                                  (TBinop (TMakePair Nat Bool) (TNConst 4) (TBConst true))
+                                                  (TBConst false))).
+Eval simpl in texpDenote (TBinop TLt (TBinop TPlus (TNConst 2)(TNConst 2)) (TNConst 7)).
 
 
+(* Compiler for stack machine *)
 
+Definition tstack := list type.
+
+(* instruction is types in the sense that it tells us the type of stack that it expects
+   and type of stacks that it will produce
+ *)
+Inductive tinstr: tstack -> tstack -> Set :=
+  | TiNConst : forall s, nat -> tinstr s (Nat :: s)
+  | TiBConst : forall s, bool -> tinstr s (Bool :: s)
+  | TiBinop : forall arg1 arg2 res s, 
+                tbinop arg1 arg2 res -> tinstr (arg1 :: arg2 :: s) (res :: s).
+  (* TODO : See if tiPairop would come *)
+
+Inductive tprog: tstack -> tstack -> Set :=
+  | TNil: forall s, tprog s s
+  | TCons: forall s1 s2 s3, tinstr s1 s2 -> tprog s2 s3 -> tprog s1 s3.
+
+Fixpoint vstack (ts: tstack): Set :=
+  match ts with
+    | nil => unit
+    | t :: ts' => typeDenote t * vstack ts'
+  end%type.
+
+
+Definition tinstrDenote ts ts' (i: tinstr ts ts'): vstack ts -> vstack ts' :=
+  match i with
+    | TiNConst _ n => fun s => (n, s)
+    | TiBConst _ b => fun s => (b, s)
+    | TiBinop _ _ _ _ b => fun s =>
+                             let '(arg1, (arg2, s')) := s in
+                             ((tbinopDenote b) arg1 arg2, s')
+    (* TODO : See if pairop would come *)
+  end.
+
+
+Fixpoint tprogDenote ts ts' (p : tprog ts ts') : vstack ts -> vstack ts' :=
+  match p with
+    | TNil _ => fun s => s
+    | TCons _ _ _ i p' => fun s => tprogDenote p' (tinstrDenote i s)
+end.
+
+
+Fixpoint tconcat ts ts' ts'' (p: tprog ts ts'): tprog ts' ts'' -> tprog ts ts'' :=
+  match p with
+    | TNil _ => fun p' => p'
+    | TCons _ _ _ i p1 => fun p' => TCons i (tconcat p1 p')
+  end.
+
+Fixpoint tcompile t (e: texp t) (ts: tstack): tprog ts (t::ts) :=
+  match e with
+    | TNConst n => TCons (TiNConst _ n) (TNil _)
+    | TBConst b => TCons (TiBConst _ b) (TNil _)
+    | TBinop _ _ _ b e1 e2 => tconcat (tcompile e2 _) 
+                                      (tconcat (tcompile e1 _) (TCons (TiBinop _ b) (TNil _)))
+    (* TODO : See if pairop would come *)
+  end.
