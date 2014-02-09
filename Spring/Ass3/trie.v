103651c0659caaa4a8485f6ffaf9276b32e6d067
Nimish Gupta <nimish28@gmail.com>
2014-02-08 20:25:53 -0500

    checkpoint

--- Spring/Ass3/trie.v
+++ Spring/Ass3/trie.v
@@ -2,27 +2,23 @@
 Require Import Bool.
 Set Implicit Arguments.
 
-Section Tries.
-
-Variable A: Set.
-
 (* XXX: Should consider defining this as list of bools *)
 Inductive bits: Set :=
   | Emptybits
   | Bit : bool -> bits -> bits.
 
-Inductive trie: Set :=
-  | Leaf: trie
-  | Node: A -> trie -> (* bits -> *) option A -> trie -> trie.
+Inductive trie (A: Set): Set :=
+  | Leaf: trie A
+  | Node: trie A -> (* bits -> *) option A -> trie A -> trie A.
 
 
 
-Fixpoint search (in_trie: trie) (key: bits) : option A :=
+Fixpoint search {A: Set} (in_trie: trie A) (key: bits) : option A :=
   match key, in_trie with
-    | Emptybits, Node _ _ val _ => val
+    | Emptybits, Node _ val _ => val
     | _, Leaf => None
-    | Bit true  bits', Node _ _ _ rtrie => search rtrie bits' 
-    | Bit false bits', Node _ ltrie _ _ => search ltrie bits'
+    | Bit true  bits', Node _ _ rtrie => search rtrie bits' 
+    | Bit false bits', Node ltrie _ _ => search ltrie bits'
   end.
 
 Check search.
@@ -32,13 +28,13 @@ Check search.
      case "Fill in missing nodes"
 *)
 
-Fixpoint insert (val: A) (in_trie: trie) (key: bits) : trie :=
-  match key, in_trie  with
-    | Emptybits, Node _ ltrie _ rtrie => Node _ ltrie (Some val) rtrie
-    | Emptybits, Leaf  => Node _ Leaf (Some val) Leaf
-    | Bit true bits', Node _ ltrie v rtrie => Node _ ltrie v (insert val rtrie bits') 
-    | Bit true bits', Leaf => Node _ Leaf None (insert val Leaf bits')
-    | Bit false bits', Node _ ltrie v rtrie => Node _ (insert val ltrie bits') v rtrie
+Fixpoint insert {A: Set} (val: A) (in_trie: trie A) (key: bits) : trie A :=
+  match key, in_trie with
+    | Emptybits, Node ltrie _ rtrie => Node ltrie (Some val) rtrie
+    | Emptybits, Leaf  => Node Leaf (Some val) Leaf
+    | Bit true bits', Node ltrie v rtrie => Node ltrie v (insert _ val rtrie bits') 
+    | Bit true bits', Leaf => Node Leaf None (insert _ val Leaf bits')
+    | Bit false bits', Node ltrie v rtrie => Node (insert _ val ltrie bits') v rtrie
     | Bit false bits', Leaf => Node _ (insert val Leaf bits') None Leaf
   end.
 
